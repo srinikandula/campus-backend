b674c0ab7d41e56fccfb9df59706877785875169
srinikandula <srini.kandula@whizzard.in>
2023-04-10 09:32:46 +0530

    Adding JUnit tests

--- src/main/java/com/anyaudit/models/Role.java
+++ src/main/java/com/anyaudit/models/Role.java
@@ -1,9 +1,14 @@
 package com.anyaudit.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "roles")
+@Getter
+@Setter
 public class Role {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,19 +26,5 @@ public class Role {
     this.name = name;
   }
 
-  public Integer getId() {
-    return id;
-  }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public ERole getName() {
-    return name;
-  }
-
-  public void setName(ERole name) {
-    this.name = name;
-  }
 }
