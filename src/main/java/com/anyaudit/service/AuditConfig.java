aa1aaa36a7a90ac358714b9bf5c4db43cae50eca
madhukant <madhukantsingh15@gmai.com>
2023-04-17 13:04:02 +0530

    missing dependance springfox

--- src/main/java/com/anyaudit/service/AuditConfig.java
+++ src/main/java/com/anyaudit/service/AuditConfig.java
@@ -1,6 +1,6 @@
 package com.anyaudit.service;
 
-import org.modelmapper.ModelMapper;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -14,10 +14,6 @@ public class AuditConfig {
     @Bean
     public AuditorAware<String> auditorAware(){return new AuditorAwareImpl();}
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 
 
 }
